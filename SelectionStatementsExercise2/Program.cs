b06096c5300f8a7280e7012d1584c8fc2917ff12
Jeff Brown <jeff@jeffbrown.tech>
2025-03-25 06:37:32 -0700

    Completed Selection Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("All those numbers make my head hurt!");
+                    break;
+                
+                case "Science":
+                    Console.WriteLine("Mitochondria is powerhouse of cell.");
+                    break;
+
+                case "History":
+                    Console.WriteLine("May you live in interesting times!");
+                    break;
+
+                case "English":
+                    Console.WriteLine("To be or not to be....what was the question?");
+                    break;
+
+                case "Band":
+                    Console.WriteLine("🎺 Choreography and Merriment!");
+                    break;
+
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    break;
+            }
         }
     }
 }
